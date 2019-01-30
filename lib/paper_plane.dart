6d5624f9fe19bc5bdcf2cb3ae908bb4850a6e4de
Kabir Kwatra <kkwatra1@saddleback.edu>
2019-01-29 21:52:59 -0800

    added Score

--- lib/paper_plane.dart
+++ lib/paper_plane.dart
@@ -1,13 +1,18 @@
 class PaperPlane {
   int _state = 0;
+  int _score = 0;
   getState() => _state;
   incrementState() {
     if (_state < 4)
       return ++_state;
+    _score++;
     return _state = 0;
   }
   getStateName() {
     return "pa" + (_state + 1).toString();
+  }
+  getScore() {
+    return _score;
   }
 
   PaperPlane();
