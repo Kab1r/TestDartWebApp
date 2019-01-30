d63025e921f1809b8519543fc95bb5e1ef0d8287
Kabir Kwatra <kkwatra1@saddleback.edu>
2019-01-29 19:27:16 -0800

    added stuff

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,13 +1,16 @@
 import 'package:angular/angular.dart';
+import 'hero.dart';
 
 @Component(
   selector: 'my-app',
   template: '''
     <h1>{{title}}</h1>
-    <h2>{{hero}}</h2>
+    <h2>{{hero.name}}</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
   ''',
 )
 class AppComponent {
   final title = 'Title';
-  var hero = 'Hero';
+  Hero hero = Hero(1, 'Hero 1');
 }
