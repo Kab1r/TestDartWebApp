79b3d1b65dc7a3ec467033226e7e3190909a9c3f
Kabir Kwatra <kkwatra1@saddleback.edu>
2019-01-29 19:04:33 -0800

    Testing

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,8 +2,12 @@ import 'package:angular/angular.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: '''
+    <h1>{{title}}</h1>
+    <h2>{{hero}}</h2>
+  ''',
 )
 class AppComponent {
-  var name = 'Angular';
+  final title = 'Title';
+  var hero = 'Hero';
 }
